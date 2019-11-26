205f81aaa524aaf1dddc5f2087fc25410d46d7bf
Alenoosh Zomorodian <alenoosh@cerenade.com>
2019-11-25 16:01:04 -0800

    Initial add

---
+++ HelloserviceHost/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace HelloserviceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (ServiceHost host = new ServiceHost(typeof(HelloService.HelloService)))
+            {
+                ServiceMetadataBehavior serviceBehavoiur = new ServiceMetadataBehavior()
+                {
+                    HttpGetEnabled = true
+                };
+
+                host.AddServiceEndpoint(typeof(HelloService.IHelloService), new BasicHttpBinding(), "HelloService");
+                host.Description.Behaviors.Add(serviceBehavoiur);
+                host.Open();
+                Console.WriteLine("Service Started @ " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
